c9b50cbe7a497ac6e9a9ae8276612d427d9dec1b
Team Teledildonics <teledildonics@hmamail.com>
2017-04-01 09:25:55 +0200

    Log incoming messages

--- src/main/java/buttplugbot/telegrambot/smack/SmackChatMessageListener.java
+++ src/main/java/buttplugbot/telegrambot/smack/SmackChatMessageListener.java
@@ -1,14 +1,24 @@
 package buttplugbot.telegrambot.smack;
 
+import org.apache.commons.codec.Charsets;
+import org.apache.commons.codec.binary.Base64;
 import org.jivesoftware.smack.chat.Chat;
 import org.jivesoftware.smack.chat.ChatMessageListener;
 import org.jivesoftware.smack.packet.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SmackChatMessageListener implements ChatMessageListener {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Override
 	public void processMessage(Chat chat, Message message) {
-		// Ignore incoming messages for now
+		String participant = chat.getParticipant();
+		String body = Base64.isBase64(message.getBody())
+				? new String(Base64.decodeBase64(message.getBody()), Charsets.UTF_8) : message.getBody();
+		String from = message.getFrom();
+		logger.info("Received message in chat with {} from {}: {}", participant, from, body);
 	}
 
 }
